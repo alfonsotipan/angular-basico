97471272d67b95e5bf127de5ec09dbb6e7f70aea
alfonsotipan <alfonso.tipan.2k9@gmail.com>
2022-10-03 19:02:22 -0500

    Mi primer Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+  //'<span>Rafael</span>'
+})
+export class AppComponent {
+  
+
+}
